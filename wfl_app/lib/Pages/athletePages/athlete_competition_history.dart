a90bb4a10b6882f777f331016b286e18fae8c32d
Job Engelen <jobengelen@yahoo.nl>
2020-06-02 16:19:07 +0200

    Athlete detail page geupdate om meer data te laten zien (WiP)
    Nog niet af

---
+++ wfl_app/lib/Pages/athletePages/athlete_competition_history.dart
@@ -0,0 +1,60 @@
+/*import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:wfl_app/model/athletes.dart';
+
+class AthletesCompPage extends StatelessWidget {
+  // Declare a field that holds the Athlete.
+  final Athlete athlete;
+
+  // In the constructor, require a Athlete.
+  AthletesCompPage({Key key, @required this.athlete}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Athlete to create the UI.
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+          title: Text(athlete.athleteFullName),
+          actions: <Widget>[],
+          backgroundColor: Colors.black),
+      //content
+      body: Container(
+        padding: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+          bottom: 10,
+          top: 0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              child: Container(
+                height: 210,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  image: DecorationImage(
+                    fit: BoxFit.fitHeight,
+                    image: NetworkImage(athlete.athletePicture),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              athlete.athleteDescription,
+              style: TextStyle(
+                color: Colors.grey[100],
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
--- wfl_app/lib/Pages/athletePages/athlete_detail_page.dart
+++ wfl_app/lib/Pages/athletePages/athlete_detail_page.dart
@@ -1,58 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:tuple/tuple.dart';
+import 'package:wfl_app/Pages/components/athlete_info_sliver_app_bar.dart';
+import 'package:wfl_app/Pages/delegates/sliver_persistent_header_delegate_impl.dart';
 import 'package:wfl_app/model/athletes.dart';
+import 'athlete_competition_history.dart';
+import 'athlete_info_page.dart';
 
-class AthletesDetailPage extends StatelessWidget {
+class AthletesDetailPage extends StatefulWidget {
   // Declare a field that holds the Athlete.
   final Athlete athlete;
 
   // In the constructor, require a Athlete.
   AthletesDetailPage({Key key, @required this.athlete}) : super(key: key);
+  
+  @override
+  _AthletesDetailPageState createState() => _AthletesDetailPageState();
+}
+
+class _AthletesDetailPageState extends State<AthletesDetailPage>
+    with SingleTickerProviderStateMixin {
+      
+  final List<Tuple3> _pages = [
+    Tuple3('Info', AthletesInfoPage(), Icon(Icons.image)),
+    /*Tuple3('Match history', AthletesCompPage(), Icon(Icons.image)),*/
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: _pages.length, vsync: this);
+    _tabController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Use the Athlete to create the UI.
     return Scaffold(
-      backgroundColor: Colors.grey[800],
-      appBar: AppBar(
-          title: Text(athlete.athleteFullName),
-          actions: <Widget>[],
-          backgroundColor: Colors.red[900]),
-      //content
-      body: Container(
-        padding: const EdgeInsets.only(
-          left: 10,
-          right: 10,
-          bottom: 10,
-          top: 0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              elevation: 10,
-              child: Container(
-                height: 210,
-                width: 1000,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(4),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(athlete.athletePicture),
-                  ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            AthleteSliverAppBar(_pages[_tabController.index].item1),
+            SliverPersistentHeader(
+              delegate: SliverPersistentHeaderDelegateImpl(
+                tabBar: TabBar(
+                  isScrollable: true,
+                  labelColor: Colors.black,
+                  indicatorColor: Colors.black,
+                  controller: _tabController,
+                  tabs: _pages
+                      .map<Tab>((Tuple3 page) => Tab(text: page.item1))
+                      .toList(),
                 ),
               ),
             ),
-            Text(
-              athlete.athleteDescription,
-              style: TextStyle(
-                color: Colors.grey[100],
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.left,
-            ),
-          ],
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: _pages.map<Widget>((Tuple3 page) => page.item2).toList(),
         ),
       ),
     );
