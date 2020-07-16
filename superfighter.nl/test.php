2b1fb4333ecd7ef6f5300ed2038c49fb15ebc28b
Job Engelen <jobengelen@yahoo.nl>
2020-07-16 12:20:57 +0200

    Testen met meerdere mails versturen
    - Meerdere mails versturen werkt, wordt maar 1 mail per mail verstuurd, dus niet alles tegelijk zodat iedereen elkaar zijn mail kan zien.

--- superfighter.nl/test.php
+++ superfighter.nl/test.php
@@ -1,29 +1,77 @@
 <?php
 $db = mysqli_connect('localhost', 'jobenam437', 'a5i3v6jf', 'jobenam437_wflapp');
 
-$today = date("Y-m-d");
-
-$only1 = true;
-$query = "SELECT * FROM `events` WHERE NOT event_name = 'event_0' AND event_date >= '$today' ORDER BY event_date ASC";
+$query = "SELECT id, naam, email FROM testmail";
 $result = mysqli_query($db, $query);
+$mailArray[] = array();
 while ($row = mysqli_fetch_assoc($result)) {
-    if ($only1) {
-        $date = $row['event_date'];
-        $year = substr($date, 0, -6);
-        $month = substr($date, 5, -3);
-        $day = substr($date, 8);
-        echo '{ ';
-        echo '"event_id": "'.$row['event_id'].'", ';
-        echo '"event_name": "'.$row['event_name'].'", ';
-        echo '"event_description": "'.$row['event_description'].'", ';
-        echo '"event_place": "'.$row['event_place'].'", ';
-        echo '"event_picture": "'.$row['event_picture'].'", ';
-        echo '"event_picture2": "'.$row['event_picture2'].'", ';
-        echo '"event_link": "'.$row['event_link'].'", ';
-        echo '"event_year": "'.$year.'", ';
-        echo '"event_month": "'.$month.'", ';
-        echo '"event_day": "'.$day.'"';
-        echo ' }';
-        $only1 = false;
+    //$id = $row['id'];
+    $mailArray[] = $row['email'];
+}
+$noMore = false;
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load composer's autoloader
+require 'vendor/autoload.php';
+
+const SECRET = "geheimpje";
+
+$i = 0;
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+foreach ($mailArray as $data) {
+    if (!$noMore) {
+        $noMore = true;
+    } else {
+        try {
+            //Server settings
+            //Enable SMTP debugging
+            // 0 = off (for production use)
+            // 1 = client messages
+            // 2 = client and server messages
+            $mail->SMTPDebug = 2;
+            $mail->isSMTP();
+            $mail->Host = gethostbyname('smtp.gmail.com');//'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'novaphptest@gmail.com';
+            $mail->Password = phptest123;
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            $mail->Port = 587;
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            //Recipients
+            $mail->setFrom('novaphptest@gmail.com', 'superfighter.nl');
+            $mail->ClearAllRecipients();
+            $mail->addAddress($data);
+
+            //Content
+            $mail->isHTML(true);
+            $mail->Subject = $data.' wil aanmelden';
+            $i++;
+            $mail->Body = '<EOF
+                   <html>
+                   <h2>Hoi hallo '.$data.'</h2>
+                   '.$data.' mail nmr: '.$i.'
+                   </html>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent to '.$data;
+            } catch (Exception $e) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        }
     }
 }
+?>
