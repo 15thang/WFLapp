071ce0b186b2264f1ac0c43dd8ac08e3eb3a14c6
Thang <thang.15@hotmail.nl>
2020-07-17 14:53:52 +0200

    Other Pages
    Version 4

--- wfl_app/lib/Pages/morePages/more_page.dart
+++ wfl_app/lib/Pages/morePages/more_page.dart
@@ -305,7 +305,21 @@ class _MorePage extends State<MorePage> {
                                 textAlign: TextAlign.left,
                               ),
                             ),
-                            Divider(color: Colors.black)
+                          ],
+                        ),
+                        margin: EdgeInsets.only(left: 8, right: 8),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              width: 10000,
+                              child: Text(
+                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                                style: TextStyle(fontSize: 15),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
                           ],
                         ),
                         margin: EdgeInsets.only(left: 8, right: 8),
