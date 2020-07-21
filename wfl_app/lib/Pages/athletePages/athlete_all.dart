63167b76e43ecb250c851cbd4a7270e8b275ebb9
Job Engelen <jobengelen@yahoo.nl>
2020-07-21 14:38:54 +0200

    Athletepages zijn nu tablet-vriendelijk

--- wfl_app/lib/Pages/athletePages/athlete_all.dart
+++ wfl_app/lib/Pages/athletePages/athlete_all.dart
@@ -51,9 +51,272 @@ class _All extends State<All> {
     super.initState();
   }
 
+  //kijk voor tablet of telefoon
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Center(
+        child: (_width > 500) ? _bigDisplay() : _smallDisplay(),
+      ),
+    );
+  }
+
+  //tablet view
+  Widget _bigDisplay() {
     return new Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 79 / 32,
+        children: List.generate(
+          _notes.length,
+          (index) {
+            String athleteWeightclass = _notes[index].athleteWeightclass;
+            switch (athleteWeightclass) {
+              case "0":
+                athleteWeightclass = "";
+                break;
+              case "1":
+                athleteWeightclass = "95+";
+                break;
+              case "2":
+                athleteWeightclass = "95";
+                break;
+              case "3":
+                athleteWeightclass = "84";
+                break;
+              case "4":
+                athleteWeightclass = "77";
+                break;
+              case "5":
+                athleteWeightclass = "70";
+                break;
+              case "6":
+                athleteWeightclass = "65";
+                break;
+              case "7":
+                athleteWeightclass = "61";
+                break;
+              case "8":
+                athleteWeightclass = "56";
+                break;
+              case "9":
+                athleteWeightclass = "52";
+                break;
+              case "10":
+                athleteWeightclass = "48";
+                break;
+              case "11":
+                athleteWeightclass = "44";
+                break;
+              case "12":
+                athleteWeightclass = "40";
+                break;
+              case "13":
+                athleteWeightclass = "36";
+                break;
+              case "14":
+                athleteWeightclass = "32";
+                break;
+            }
+            //switch case om de nummers naar letters te veranderen
+            String athleteGrade = _notes[index].athleteGrade;
+            switch (athleteGrade) {
+              case "0":
+                athleteGrade = "";
+                break;
+              case "1":
+                athleteGrade = "A";
+                break;
+              case "2":
+                athleteGrade = "B";
+                break;
+              case "3":
+                athleteGrade = "C";
+                break;
+              case "4":
+                athleteGrade = "N";
+                break;
+              case "5":
+                athleteGrade = "J";
+                break;
+            }
+            //Als ster 1 is, wordt het een ster
+            String athleteStar = _notes[index].athleteStar.toString();
+            if (athleteStar == "1") {
+              athleteStar = "★";
+            }
+            return new GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AthletesDetailPage(
+                      athleteId: int.parse(_notes[index].athleteId),
+                      athleteFullName: _notes[index].athleteFullName,
+                      athleteWins: int.parse(_notes[index].athleteWins),
+                      athleteLosses: int.parse(_notes[index].athleteLosses),
+                      athleteDraws: int.parse(_notes[index].athleteDraws),
+                      athleteYellowcards:
+                          int.parse(_notes[index].totalYellowcards),
+                      athleteRedcards: int.parse(_notes[index].totalRedcards),
+                    ),
+                  ),
+                );
+              },
+              child: new Card(
+                color: Colors.grey[400],
+                elevation: 10.0,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: 174,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(5),
+                              topLeft: Radius.circular(5)),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 11,
+                              child: Stack(
+                                children: <Widget>[
+                                  Container(
+                                    width: 174,
+                                    height: 174,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(5),
+                                          topLeft: Radius.circular(5)),
+                                      image: new DecorationImage(
+                                          image: new NetworkImage(_notes[index]
+                                              .athleteProfilePicture),
+                                          fit: BoxFit.cover),
+                                    ),
+                                    child: Container(
+                                      alignment: Alignment.bottomCenter,
+                                      child: Text(
+                                        _notes[index].athleteTitle,
+                                        style: TextStyle(
+                                            color: Colors.yellow,
+                                            backgroundColor: Colors.red[900]),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 174,
+                                    height: 174,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      gradient: LinearGradient(
+                                        begin: FractionalOffset.topCenter,
+                                        end: FractionalOffset.bottomCenter,
+                                        colors: [
+                                          Colors.grey.withOpacity(0.0),
+                                          Colors.black.withOpacity(0.5)
+                                        ],
+                                        stops: [0.0, 1.0],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 14,
+                              child: Container(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(_notes[index].athleteFullName,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17)),
+                                    Text(_notes[index].athleteNickname,
+                                        style: TextStyle(
+                                            fontStyle: FontStyle.italic,
+                                            fontSize: 17)),
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 5.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(_notes[index].athleteNationality,
+                                              style: TextStyle(fontSize: 16)),
+                                          Text(_notes[index].athleteDayOfBirth,
+                                              style: TextStyle(fontSize: 16)),
+                                          Container(
+                                            margin:
+                                                const EdgeInsets.only(top: 5.0),
+                                            child: Text(
+                                                athleteWeightclass +
+                                                    '"  ' +
+                                                    athleteGrade,
+                                                style: TextStyle(fontSize: 16)),
+                                          ),
+                                          Container(
+                                            margin:
+                                                const EdgeInsets.only(top: 7.0),
+                                            child: Row(
+                                              children: <Widget>[
+                                                Text(athleteStar,
+                                                    style: TextStyle(
+                                                      fontSize: 22,
+                                                      color: Colors.yellow[700],
+                                                      shadows: <Shadow>[
+                                                        Shadow(
+                                                            offset: Offset(
+                                                                0.0, 0.0),
+                                                            blurRadius: 3.0,
+                                                            color:
+                                                                Colors.black),
+                                                      ],
+                                                    )),
+                                                Container(
+                                                  margin: const EdgeInsets.only(
+                                                      top: 5.0),
+                                                  child: Text(
+                                                      _notes[index]
+                                                          .athleteStars,
+                                                      style: TextStyle(
+                                                          fontSize: 17)),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _smallDisplay() {
+    return Scaffold(
       backgroundColor: Colors.grey[800],
       body: ListView.builder(
         itemBuilder: (context, index) {
@@ -143,7 +406,8 @@ class _All extends State<All> {
                     athleteWins: int.parse(_notes[index].athleteWins),
                     athleteLosses: int.parse(_notes[index].athleteLosses),
                     athleteDraws: int.parse(_notes[index].athleteDraws),
-                    athleteYellowcards: int.parse(_notes[index].totalYellowcards),
+                    athleteYellowcards:
+                        int.parse(_notes[index].totalYellowcards),
                     athleteRedcards: int.parse(_notes[index].totalRedcards),
                   ),
                 ),
