f2a35c958bfd2129acec212a1d2b041193f7846e
Job Engelen <jobengelen@yahoo.nl>
2020-07-30 13:11:30 +0200

    Email moet email zijn voor nieuwsbrief

--- superfighter.nl/APP_insert_newsmail.php
+++ superfighter.nl/APP_insert_newsmail.php
@@ -3,15 +3,23 @@ $db = mysqli_connect('localhost', 'jobenam437', 'a5i3v6jf', 'jobenam437_wflapp')
 
 $email = $_POST['email'];
 
-$query = "INSERT INTO newsmail (email)
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+// Email correct
+    $query = "INSERT INTO newsmail (email)
   			  VALUES('$email')";
-$results = $db->prepare($query);
+    $results = $db->prepare($query);
+    $results->execute();
 
-$results->execute();
-
-if($results>0)
-{
-    echo json_encode("Subscribed succesfully");
+    if($results>0)
+    {
+        echo json_encode("Subscribed succesfully");
+    } else {
+        echo json_encode("Could not subscribe");
+    }
+}
+else {
+//Email not correct
+    echo json_encode("Could not subscribe");
 }
 
 ?>
