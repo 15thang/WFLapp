650a25a7a22c40b43397d5ee29214cfdd07f7817
Job <jobengelen@yahoo.nl>
2020-08-28 09:53:48 +0200

    morepage edit en screenshots edit
    -Algemene voorwaarde verwijderd uit app
    -Algemene voorwaarde uit screenshots geweizigd

--- wfl_app/lib/Pages/morePages/more_page.dart
+++ wfl_app/lib/Pages/morePages/more_page.dart
@@ -284,66 +284,6 @@ class _MorePage extends State<MorePage> {
                         ),
                         margin: EdgeInsets.only(left: 8, right: 8),
                       ),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 5,
-                            child: Container(
-                              height: 5,
-                              color: Colors.redAccent[700],
-                              margin: EdgeInsets.only(left: 8),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 5,
-                            child: Container(
-                              height: 5,
-                              color: Colors.blue[900],
-                              margin: EdgeInsets.only(right: 8),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              width: 10000,
-                              child: Text(
-                                'Terms of Service:',
-                                style: TextStyle(fontSize: 25),
-                                textAlign: TextAlign.left,
-                              ),
-                            ),
-                          ],
-                        ),
-                        margin: EdgeInsets.only(left: 8, right: 8),
-                      ),
-                      Container(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              width: 10000,
-                              child: Text(
-                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                                style: TextStyle(fontSize: 15),
-                                textAlign: TextAlign.left,
-                              ),
-                            ),
-                          ],
-                        ),
-                        margin: EdgeInsets.only(left: 8, right: 8),
-                      ),
-                      Container(
-                        height: 55,
-                        width: 170,
-                        decoration: BoxDecoration(
-                          image: new DecorationImage(
-                              image: new NetworkImage(
-                                  'https://wfltickets.com/wp-content/uploads/2018/08/world-fighting-league-logo.png'),
-                              fit: BoxFit.fill),
-                        ),
-                      ),
                     ],
                   ),
                 ),
